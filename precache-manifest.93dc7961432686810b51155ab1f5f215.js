2e11da699893ed948e1602fa3ac8b68f0c634edd
Kevin <kev.saintigny.schmidt@gmail.com>
2020-09-21 08:54:00 +0200

    Updates

---
+++ precache-manifest.93dc7961432686810b51155ab1f5f215.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "177b4336fb8bcd65498cc1f9a0e5f534",
+    "url": "./react--portfolio/index.html"
+  },
+  {
+    "revision": "bbbababa0ed54b8d347c",
+    "url": "./react--portfolio/static/css/main.53d97c50.chunk.css"
+  },
+  {
+    "revision": "5aa6d70092772df2900f",
+    "url": "./react--portfolio/static/js/2.ba4becdf.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "./react--portfolio/static/js/2.ba4becdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbbababa0ed54b8d347c",
+    "url": "./react--portfolio/static/js/main.4ef42c59.chunk.js"
+  },
+  {
+    "revision": "80551381616a59b6d79f",
+    "url": "./react--portfolio/static/js/runtime-main.167602ed.js"
+  },
+  {
+    "revision": "aea8850633a4a4494cb402c573cb3efe",
+    "url": "./react--portfolio/static/media/avatar.aea88506.jpg"
+  },
+  {
+    "revision": "ad709587e733bcd86594cdca98c933a7",
+    "url": "./react--portfolio/static/media/movie-selection.ad709587.png"
+  },
+  {
+    "revision": "e7effbd8ab80b77002b46b211aecb2a2",
+    "url": "./react--portfolio/static/media/paris.e7effbd8.png"
+  },
+  {
+    "revision": "6132e1e686054dd9681e13285da9d5a2",
+    "url": "./react--portfolio/static/media/todolist.6132e1e6.png"
+  },
+  {
+    "revision": "26e868d72fad22493645391ded422103",
+    "url": "./react--portfolio/static/media/vinyl-store.26e868d7.png"
+  }
+]);
